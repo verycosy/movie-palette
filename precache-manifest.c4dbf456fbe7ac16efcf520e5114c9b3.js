10f0a8946c75b3471cc8ddcc452f18d7da0fdae0
positive-jinho <red_horse_@naver.com>
2019-04-08 15:00:07 +0900

    Updates

---
+++ precache-manifest.c4dbf456fbe7ac16efcf520e5114c9b3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "bd0f28d1052f00e19f27",
+    "url": "/movie-palette/static/js/runtime~main.1cd8e1fe.js"
+  },
+  {
+    "revision": "6e34e6cb5189547d82f4",
+    "url": "/movie-palette/static/js/main.bc0643ae.chunk.js"
+  },
+  {
+    "revision": "73ec5b5ad42fd4c1b9a4",
+    "url": "/movie-palette/static/js/2.94b533a9.chunk.js"
+  },
+  {
+    "revision": "6e34e6cb5189547d82f4",
+    "url": "/movie-palette/static/css/main.018b3f49.chunk.css"
+  },
+  {
+    "revision": "9e08ec8fea4c9b38af200f1e947bfb03",
+    "url": "/movie-palette/index.html"
+  }
+];
