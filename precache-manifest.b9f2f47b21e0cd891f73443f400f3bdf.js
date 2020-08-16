ad05eb04fc5c0234e3357f6dad8f0bda6ba43460
verycosy <verycosy@kakao.com>
2020-08-16 23:33:29 +0900

    Updates

---
+++ precache-manifest.b9f2f47b21e0cd891f73443f400f3bdf.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "41af80fadb1bd47d2391b73ed755dde1",
+    "url": "/movie-palette/index.html"
+  },
+  {
+    "revision": "59cceb27d7f85783a299",
+    "url": "/movie-palette/static/js/2.8c08568d.chunk.js"
+  },
+  {
+    "revision": "60536cc9220fa45b69dbd7024ced8ad7",
+    "url": "/movie-palette/static/js/2.8c08568d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5d7a7455259dbebe0bc0",
+    "url": "/movie-palette/static/js/main.35fa30dd.chunk.js"
+  },
+  {
+    "revision": "dd0338f2ce6e219dc0ec",
+    "url": "/movie-palette/static/js/runtime-main.211ed1a3.js"
+  }
+]);
